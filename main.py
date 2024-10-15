a5cbc7daa4055c9a4a745b73dead7f0905c7883b
Matteus Pennala <matteuspennala@gmail.com>
2024-10-15 20:18:14 +0300

    4. Player movement and object grouping

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
     dt = 0
 
     player = Player(x=SCREEN_WIDTH/2,y=SCREEN_HEIGHT/2)
@@ -16,7 +20,15 @@ def main():
                 return
         
         screen.fill("black")
-        player.draw(screen)
+        #player.draw(screen)
+        #player.update(dt)
+        
+        for to_update in updatable:
+            to_update.update(dt)
+
+        for to_draw in drawable:
+            to_draw.draw(screen)
+
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 if __name__ == "__main__":
--- player.py
+++ player.py
@@ -6,7 +6,6 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, radius=PLAYER_RADIUS)
         self.rotation = 0
-        #pygame.draw.polygon(screen, "white", self.triangle, 2)
         
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
@@ -19,3 +18,22 @@ class Player(CircleShape):
         b = self.position - forward * self.radius - right
         c = self.position - forward * self.radius + right
         return [a, b, c]
+    
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(dt * -1)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(dt * -1)
+    
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
